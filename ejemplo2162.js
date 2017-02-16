1d581dfb20b968b0d374ef62e90ea2d44694960c
2013640093 <hernandezmiguelan@gmail.com>
2017-02-16 13:46:52 -0600

    Update ejemplo2162.js

--- ejemplo2162.js
+++ ejemplo2162.js
@@ -1,4 +1,4 @@
 var mi Canvas= document.getElementById("miCanvas");
 var contexto=miCanvas.getContext("2d");
 contexto.fillStyle="red";
-contexto.fillRECT(30,30,50,50);
+contexto.fillRect(30,30,50,50);
