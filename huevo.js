ca679e7f2e10d4688c0ecb56c2fad35e75ac68eb
2013640093 <hernandezmiguelan@gmail.com>
2017-02-28 13:45:43 -0600

    Update huevo.js

--- huevo.js
+++ huevo.js
@@ -1,7 +1,7 @@
 
 var puntos = [];
 for ( var i = 0; i < 50; i ++ ) {
-    puntos.push( new THREE.Vector2(Math.sqrt(i*i/Math.sqrt(i)-i*i),( i - 5 ) * 2 ));
+    puntos.push( new THREE.Vector2(Math.sqrt(i*i/Math.sqrt(i)-i*i)*15,( i - 5 ) * 2 ));
 }
 var forma = new THREE.LatheGeometry(puntos);
 var material = new THREE.MeshNormalMaterial();
