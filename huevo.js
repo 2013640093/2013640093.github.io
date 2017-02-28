d92615bb5339ce08daea714b232cac682bdde850
2013640093 <hernandezmiguelan@gmail.com>
2017-02-28 14:33:48 -0600

    Update huevo.js

--- huevo.js
+++ huevo.js
@@ -7,11 +7,12 @@ for ( var i = 0; i < 100;  i++ ) {
     //puntos.push( new THREE.Vector3(Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i),-i));
     //puntos.push( new THREE.Vector3(i,Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i)));
 }
+var color = new THREE.Color("rgb(245,245,220)");
 var forma = new THREE.LatheGeometry(puntos);
-//var material = new THREE.MeshNormalMaterial();
-var color=new THREE.Color("rgb(245,245,220)");
-//var malla = new THREE.Mesh( forma, material,color );
-var malla = new THREE.Mesh( forma, color );
+var material = new THREE.MeshNormalMaterial(color);
+
+var malla = new THREE.Mesh( forma, material);
+
 malla.rotateZ( Math.PI );
 var escena = new THREE.Scene();
 escena.add(malla);
