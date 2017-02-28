9025a5b505ed49a08ec3280908aa33a13ae06113
2013640093 <hernandezmiguelan@gmail.com>
2017-02-28 14:31:44 -0600

    Update huevo.js

--- huevo.js
+++ huevo.js
@@ -2,7 +2,7 @@
 var puntos = [];
 for ( var i = 0; i < 100;  i++ ) {
     
-    puntos.push( new THREE.Vector2(Math.sqrt(10*i*i/Math.sqrt(i)-i*i),i));
+    puntos.push( new THREE.Vector2(Math.sqrt(10*i*i/Math.sqrt(i)-i*i),-i));
     //Modificar el signo de z, voltea la el ovalo en y
     //puntos.push( new THREE.Vector3(Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i),-i));
     //puntos.push( new THREE.Vector3(i,Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i)));
