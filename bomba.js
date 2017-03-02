ecd5f995dc1b1c368a92a3b449ef0c4fb485bdf7
2013640093 <hernandezmiguelan@gmail.com>
2017-03-02 09:41:39 -0600

    Update bomba.js

--- bomba.js
+++ bomba.js
@@ -12,20 +12,20 @@ var base = new THREE.CylinderGeometry( 2, 2, 4, 32 );
 base.translate(0,10,0);
 
 var figura = new THREE.Shape();
-figura.moveTo(3, -5);
-figura.lineTo(5, -5);
+figura.moveTo(4, -6);
 figura.lineTo(5, -6);
-figura.lineTo(2, -6);
-figura.lineTo(2, -5);
+figura.lineTo(5, -6.5);
+figura.lineTo(4, -6.5);
+figura.lineTo(4, -6);
 var pied = new THREE.ExtrudeGeometry( figura,
                                        {amount: 5} );
 
 var figura2 = new THREE.Shape();
-figura2.moveTo(-3, -5);
-figura2.lineTo(-5, -5);
+figura2.moveTo(-4, -6);
 figura2.lineTo(-5, -6);
-figura2.lineTo(-3, -6);
-figura2.lineTo(-3, -5);
+figura2.lineTo(-5, -6.5);
+figura2.lineTo(-4, -6.5);
+figura2.lineTo(-4, -6);
 var piei = new THREE.ExtrudeGeometry( figura2,
                                        {amount: 5} );
 var material = new THREE.MeshNormalMaterial();
