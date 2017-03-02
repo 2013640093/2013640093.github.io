b593ee8bbe75c2d035c532c8675558d353d9490a
2013640093 <hernandezmiguelan@gmail.com>
2017-03-02 07:03:12 -0600

    Update egg.js

--- egg.js
+++ egg.js
@@ -7,13 +7,11 @@ for ( var i = 0; i < 100;  i++ ) {
     //puntos.push( new THREE.Vector3(Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i),-i));
     //puntos.push( new THREE.Vector3(i,Math.sqrt(10*(i)*(i)/Math.sqrt(i)-i*i)));
 }
-//var color = new THREE.Color("rgb(245,245,220)");
+var color = new THREE.Color("rgb(245,245,220)");
 var forma = new THREE.LatheGeometry(puntos);
 //var material = new THREE.MeshNormalMaterial();
-var material= new TREE.MeshBasicmATERIAL({color: FFFF99});
-
+var material= new TREE.MeshBasicmATERIAL(color);
 var malla = new THREE.Mesh( forma, material);
-
 malla.rotateZ( Math.PI );
 var escena = new THREE.Scene();
 escena.add(malla);
