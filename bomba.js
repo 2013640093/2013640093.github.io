dc139ea9a701e28349f5d253cd2e49268afb0e46
2013640093 <hernandezmiguelan@gmail.com>
2017-03-02 08:52:25 -0600

    Update bomba.js

--- bomba.js
+++ bomba.js
@@ -1,15 +1,15 @@
-//var cuerpo = new THREE.SphereBufferGeometry(10,64,64);
+var cuerpo = new THREE.SphereBufferGeometry(10,64,64);
 var cuerpo = new THREE.SphereGeometry(10,64,64);
-//var ojoi=new THREE.SphereBufferGeometry(10,32,32);
+var ojoi=new THREE.SphereBufferGeometry(10,32,32);
 var ojoi=new THREE.SphereGeometry(10,32,32);
-//ojoi.translate(-2,2,5);
-//var ojod=new THREE.SphereBufferGeometry(10,32,32);
+ojoi.translate(-2,2,5);
+var ojod=new THREE.SphereBufferGeometry(10,32,32);
 var ojod=new THREE.SphereGeometry(10,32,32);
-//ojod.translate(2,2,5);
+ojod.translate(2,2,5);
 var mecha = new THREE.CylinderGeometry( 1, 1, 25, 32 );
-//mecha.translate(0,14,0);
+mecha.translate(0,14,0);
 var base = new THREE.CylinderGeometry( 2, 2, 4, 32 );
-//base.translate(0,14,0);
+base.translate(0,14,0);
 
 var figura = new THREE.Shape();
 figura.moveTo(2, -3);
@@ -30,7 +30,7 @@ var piei = new THREE.ExtrudeGeometry( figura2,
                                        {amount: 5} );
 
 var material = new THREE.MeshBasicMaterial( {color: 0xfff99} );
-var material2 = new THREE.MeshBasicMaterial( {color: 0xfff00} );
+//var material2 = new THREE.MeshBasicMaterial( {color: 0xfff00} );
 
 var mallacuerpo =new THREE.Mesh( cuerpo, material);
 var mallojoi =new THREE.Mesh( ojoi, material);
@@ -41,13 +41,13 @@ var mallapied =new THREE.Mesh( pied, material);
 var mallapiei =new THREE.Mesh( piei, material);
 
 var bomba = new THREE.Geometry();
-bomba.merge(mallacuerpo.geometry, mallacuerpo.matrix);
-bomba.merge(mallojoi.geometry, mallojoi.matrix);
-bomba.merge(mallojod.geometry, mallojod.matrix);
+//bomba.merge(mallacuerpo.geometry, mallacuerpo.matrix);
+//bomba.merge(mallojoi.geometry, mallojoi.matrix);
+//bomba.merge(mallojod.geometry, mallojod.matrix);
 bomba.merge(mallamecha.geometry, mallamecha.matrix);
-bomba.merge(mallabase.geometry, mallabase.matrix);
-bomba.merge( mallapied.geometry,  mallapied.matrix);
-bomba.merge( mallapiei.geometry,  mallapiei.matrix);
+//bomba.merge(mallabase.geometry, mallabase.matrix);
+//bomba.merge( mallapied.geometry,  mallapied.matrix);
+//bomba.merge( mallapiei.geometry,  mallapiei.matrix);
 
 var mallabomba = new THREE.Mesh(bomba, material);
 
