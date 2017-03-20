6b097934079e27086c1100698bebcfa65b99ba5b
2013640093 <hernandezmiguelan@gmail.com>
2017-03-20 12:42:09 -0600

    Update tablero1.js

--- tablero1.js
+++ tablero1.js
@@ -1,30 +1,5 @@
-///////////////TABLERO////////////////
-var escena = new THREE.Scene();
- var color=0;
-    for(var i=0;i<8;i++){
-      for(var j=0;j<8;j++){
-        //var cuboForma=  new THREE.BoxGeometry(10,5,10);
-        //cuboForma.translate(-35+i*10,0,35-j*10);
-         var cuboForma=  new THREE.BoxGeometry(20,5,20);
-        cuboForma.translate(-70+i*20,0,70-j*20);
-        if(color%2!==0){
-          //var material = new THREE.MeshBasicMaterial({color: 0xcccccc});
-         var material = new THREE.MeshBasicMaterial({color: 0xcc0000});
-        }else{
-          //var material = new THREE.MeshBasicMaterial({color: 0x555555});
-         var material = new THREE.MeshBasicMaterial({color: 0x004c99});
-        }
-        var cuboMalla = new THREE.Mesh(cuboForma,material);
-        color=color+1;
-          cuboMalla.receiveShadow=true;
-          cuboMalla.rotateY( Math.PI/4 );
-          cuboMalla.rotateZ( Math.PI/8 );
-        escena.add(cuboMalla);
-      }
-      color=color+1;
-    }    
+ 
 /////////////BOMBA////////////////
-
 //var cuerpo = new THREE.SphereBufferGeometry(10,64,64);
 var cuerpo = new THREE.SphereGeometry(10,64,64);
 //var ojoi=new THREE.SphereBufferGeometry(10,32,32);
@@ -55,15 +30,15 @@ figura2.lineTo(-7, -6.1);
 figura2.lineTo(-7, -6);
 var piei = new THREE.ExtrudeGeometry( figura2,
                                        {amount: 2} );
-var material = new THREE.MeshNormalMaterial();
-
-var mallacuerpo =new THREE.Mesh( cuerpo, material);
-var mallojoi =new THREE.Mesh( ojoi, material);
-var mallojod =new THREE.Mesh( ojod, material);
-var mallamecha =new THREE.Mesh( mecha, material);
-var mallabase =new THREE.Mesh( base, material);
-var mallapied =new THREE.Mesh( pied, material);
-var mallapiei =new THREE.Mesh( piei, material);
+//var material = new THREE.MeshNormalMaterial();
+var material1 = new THREE.MeshBasicMaterial({color: 0x202020});
+var mallacuerpo =new THREE.Mesh( cuerpo, material1);
+var mallojoi =new THREE.Mesh( ojoi, material1);
+var mallojod =new THREE.Mesh( ojod, material1);
+var mallamecha =new THREE.Mesh( mecha, material1);
+var mallabase =new THREE.Mesh( base, material1);
+var mallapied =new THREE.Mesh( pied, material1);
+var mallapiei =new THREE.Mesh( piei, material1);
 
 var bomba = new THREE.Geometry();
 bomba.merge(mallacuerpo.geometry, mallacuerpo.matrix);
@@ -74,15 +49,38 @@ bomba.merge(mallabase.geometry, mallabase.matrix);
 bomba.merge( mallapied.geometry,  mallapied.matrix);
 bomba.merge( mallapiei.geometry,  mallapiei.matrix);
 bomba.translate(50,10,50);
-var mallabomba = new THREE.Mesh(bomba, material);
+var mallabomba = new THREE.Mesh(bomba, material1);
 mallabomba.rotateY( Math.PI/4 );
 mallabomba.rotateZ( Math.PI/4 );
 var escena = new THREE.Scene();
 escena.add(mallabomba);
 
-
+///////////////TABLERO////////////////
+var escena = new THREE.Scene();
+ var color=0;
+    for(var i=0;i<8;i++){
+      for(var j=0;j<8;j++){
+        //var cuboForma=  new THREE.BoxGeometry(10,5,10);
+        //cuboForma.translate(-35+i*10,0,35-j*10);
+         var cuboForma=  new THREE.BoxGeometry(20,5,20);
+        cuboForma.translate(-70+i*20,0,70-j*20);
+        if(color%2!==0){
+          //var material = new THREE.MeshBasicMaterial({color: 0xcccccc});
+         var material = new THREE.MeshBasicMaterial({color: 0xcc0000});
+        }else{
+          //var material = new THREE.MeshBasicMaterial({color: 0x555555});
+         var material = new THREE.MeshBasicMaterial({color: 0x004c99});
+        }
+        var cuboMalla = new THREE.Mesh(cuboForma,material);
+        color=color+1;
+          cuboMalla.receiveShadow=true;
+          cuboMalla.rotateY( Math.PI/4 );
+          cuboMalla.rotateZ( Math.PI/8 );
+        escena.add(cuboMalla);
+      }
+      color=color+1;
+    }   
 ////////////////
-
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 200;
 
