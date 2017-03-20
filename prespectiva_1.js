4102c5d4b5b92a9dfad40497508d38e63f7987ad
2013640093 <hernandezmiguelan@gmail.com>
2017-03-20 16:01:00 -0600

    Update prespectiva_1.js

--- prespectiva_1.js
+++ prespectiva_1.js
@@ -6,7 +6,8 @@ var relacionAspecto =window.innerWidth / window.innerHeight;
 var planoCercano =1;
 var planoLejano =1000;
 var camara = new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
-camara.position.z= 350;
+camara.position.z= 150;
+Tabla(); 
 //////////////////
  var escena = new THREE.Scene();
 ///////////////TABLERO////////////////
