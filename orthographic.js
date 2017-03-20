be88ca499540faed47785548b4c15d8b1be64516
2013640093 <hernandezmiguelan@gmail.com>
2017-03-20 08:49:24 -0600

    Update orthographic.js

--- orthographic.js
+++ orthographic.js
@@ -1,27 +1,32 @@
-var camara = new THREE. OrthographicCamera();
-camara.left = window.innerWidth/-2;
-camara.right=window.innerWidth/2;
-camara.top=window.innerHeight/2;
-camara.bottom=window.innerHeight/-2;
-camara.near=0.1;
-camara.far=1000;
+var camara = new THREE.OrtographicCamera();
+
+camara.left = window.innerWidth / -2;
+camara.right = window.innerWidth / 2;
+camara.top = window.innerHeigth / 2;
+camara.bottom = window.innerHeigth / -2;
+camara.near = 0.1;
+camara.far = 1000;
 camara.updateProjectionMatrix();
-camara.position.z=100;
 
-var cubo= new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
+camara.position.z = 100;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeometry(30, 30, 30 ),
+                           new THREE.MeshNormalMateral() );
 cubo.rotateY(Math.PI/4);
-var esfera1= new THREE.Mesh(new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial());
+var esfera1 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMAterial() );
 esfera1.position.x=50;
-var esfera2= new THREE.Mesh(new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial());
+
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 15 ), new THREE.MeshNormalMAterial() );
 esfera2.position.x=-50;
-esfera2.position.x=-100;
+esfera2.position.z=-100;
 
-var escena= new THREE.Scene();
-esena.add(esfera1);
-esena.add(esfera2);
-esena.add(cubo);
+var escena = new THREE.Scene();
+escena.add(esfera1);
+escena.add(esfera2);
+escena.add(cubo);
 
-var renderizador=new THREE.WebGLRenderer();
-renderizador.setSize(window.innerWidth,window.innerHeight);
-document.body.appendChild(renderizador.domElement);
-renderizador.render(escena,camara);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild( renderizador.domElement );
+renderizador.render(escena, camara);
