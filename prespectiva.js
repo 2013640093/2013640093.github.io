96ac7df4171d011b835e409f2cf0f757887544ac
2013640093 <hernandezmiguelan@gmail.com>
2017-03-20 09:10:58 -0600

    Update prespectiva.js

--- prespectiva.js
+++ prespectiva.js
@@ -4,11 +4,15 @@ var planoCercano =1;
 var planoLejano =1000;
 var camara = new THREE.PerspectiveCamera(campoVision,RelacionAspecto,planoCercano,planoLejano);
 camara.position.z= 15;
-var cubo= new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial());
+
+var material = new THREE.MeshNormalMaterial();
+var cubo= new THREE.Mesh(new THREE.BoxGeometry(2,2,2),material);
 cubo.rotateY(Math.PI/4);
-var esfera1= new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
+
+var esfera1= new THREE.Mesh(new THREE.SphereGeometry(1), material);
 esfera1.position.x=5;
-var esfera2= new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial());
+
+var esfera2= new THREE.Mesh(new THREE.SphereGeometry(1), material);
 esfera2.position.x=-5;
 esfera2.position.y=-10;
 
@@ -19,4 +23,5 @@ esena.add(cubo);
 
 var renderizador=new THREE.WebGLRenderer();
 renderizador.setSize(window.innerWidth,window.innerHeight);
-document.body.app
+document.body.appendChild( renderizador.domElement );
+renderizador.render(escena, camara);
