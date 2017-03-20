23f89a576251dca5868b55e85c72fac9dc6e3de5
2013640093 <hernandezmiguelan@gmail.com>
2017-03-20 16:58:13 -0600

    Update prespectiva_2.js

--- prespectiva_2.js
+++ prespectiva_2.js
@@ -1,9 +1,9 @@
 //var campoVision =45; //grados
 //var campoVision =90; //grados
-var campoVision =-30; //grados
+var campoVision =30; //grados
 
 var relacionAspecto =window.innerWidth / window.innerHeight;
-var planoCercano =0.5;
+var planoCercano =0.8;
 var planoLejano =600;
 var camara = new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
 camara.position.z=300;
