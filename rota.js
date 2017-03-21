d3073be37a51ad5403d3db1c2fa7e5ba60d13445
2013640093 <hernandezmiguelan@gmail.com>
2017-03-21 14:18:56 -0600

    Create rota.js

---
+++ rota.js
@@ -0,0 +1,19 @@
+function setup(){
+	escena= new TRHEE.Scene();
+	camara= new THREE.PrespectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+camara.position.z=5;
+renderer= new THREE.WebGLRRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+malla= new.THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial());
+escena.add(malla);
+}
+function loop(){
+requestAnimationFrame(loop);
+malla.rotation.x += 0.01;
+malla.rotation.y += 0.01;
+renderer.renderer(escena,camara);
+}
+
+setup();
+loop();
