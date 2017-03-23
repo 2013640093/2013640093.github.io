fded6ffff2676c6dfad49c9c48bdcb5287bdc1f1
2013640093 <hernandezmiguelan@gmail.com>
2017-03-23 13:30:29 -0600

    Update rota.js

--- rota.js
+++ rota.js
@@ -1,3 +1,8 @@
+fucntion listener(){
+camara.aspect=window.innerWidth/window.innerHeight;
+	camara.uptadeProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
 function setup(){
 	escena= new THREE.Scene();
 	camara= new THREE.PrespectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
@@ -5,7 +10,9 @@ camara.position.z=5;
 renderer= new THREE.WebGLRRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-malla= new.THREE.Mesh(new new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
+	//cubo=new THREE.BoxGeometry(1,1,1)
+	//material=new THREE.MeshNormalMaterial()
+malla= new.THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshNormalMaterial());
 escena.add(malla);
 }
 function loop(){
@@ -14,6 +21,6 @@ malla.rotation.x += 0.01;
 malla.rotation.y += 0.01;
 renderer.renderer(escena,camara);
 }
-
 setup();
 loop();
+listener();
