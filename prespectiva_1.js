cb229df92a4658c898780bbda1235ee7869f517d
2013640093 <hernandezmiguelan@gmail.com>
2017-05-01 10:00:10 +0100

    Update prespectiva_1.js

--- prespectiva_1.js
+++ prespectiva_1.js
@@ -1,6 +1,6 @@
 //var campoVision =45; //grados
 //var campoVision =90; //grados
-var campoVision =90; //grados
+var campoVision =60; //grados
 
 var relacionAspecto =window.innerWidth / window.innerHeight;
 var planoCercano =0.1;
