c846791a4b4d3a356a0ef1071ebf2698388f8a0b
2013640093 <hernandezmiguelan@gmail.com>
2017-06-11 18:34:36 +0100

    Create bomba.js

--- bomba.js
+++ bomba.js
@@ -3,9 +3,9 @@ var relacionAspecto =window.innerWidth / window.innerHeight;
 var planoCercano =0.2;
 var planoLejano =1000;
 var camara = new THREE.PerspectiveCamera(campoVision,relacionAspecto,planoCercano,planoLejano);
-camara.position.x = 100;    
-camara.position.y = 200;
-camara.position.z = 100;   
+camara.position.x = 500;    
+camara.position.y = 90;
+camara.position.z = 50;   
 camara.lookAt(new THREE.Vector3(0,0,0));
 
 
