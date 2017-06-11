92f604f593b79ba64b86848e82158c6d6e64f269
2013640093 <hernandezmiguelan@gmail.com>
2017-06-11 15:16:22 -0500

    Create prototipo.js

--- prototipo.js
+++ prototipo.js
@@ -229,10 +229,9 @@ function setup(){
   environment = new Environment();
   environment.setMap( mapa );
   environment.setMapPiece( pieza );
-  camara = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, .7, 1000 );
+  camara = new THREE.PerspectiveCamera(30, window.innerWidth/window.innerHeight, .7, 1000 );
   camara.position.z =150;
   camara.position.y =-500;
-  camara.position.x =150;
   camara.lookAt( new THREE.Vector3( 0, 0, 0 ) );
   renderer = new THREE.WebGLRenderer();
   //renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
