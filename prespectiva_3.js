0216bb79fcc03ad63bd4c467d829770ffdaa1d93
2013640093 <hernandezmiguelan@gmail.com>
2017-06-11 18:53:31 +0100

    Create prespectiva_3.js

--- prespectiva_3.js
+++ prespectiva_3.js
@@ -132,7 +132,7 @@ bomba.merge(mallabase.geometry, mallabase.matrix);
 bomba.merge( mallapied.geometry,  mallapied.matrix);
 bomba.merge( mallapiei.geometry,  mallapiei.matrix);
 bomba.merge(mallabot.geometry, mallabot.matrix);
-bomba.translate(-76,10,70);
+bomba.translate(-76,15,70);
 var mallabomba = new THREE.Mesh(bomba, matext);
 //mallabomba.rotateY( Math.PI/12 );
 //mallabomba.rotateZ( Math.PI/4 );
