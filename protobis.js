6d1a6696ea9e0f700119e289da568568c5bf9335
2013640093 <hernandezmiguelan@gmail.com>
2017-06-14 18:02:44 -0500

    Create protobis.js

--- protobis.js
+++ protobis.js
@@ -625,10 +625,10 @@ camara.position.z = -300;
 camara.lookAt(new THREE.Vector3(0,0,0));*/
   
   
-  camara = new THREE.PerspectiveCamera(30, (window.innerWidth/window.innerHeight),0.2, 1000 );
+  camara = new THREE.PerspectiveCamera(50, (window.innerWidth/window.innerHeight),0.2, 1000 );
   //camara.position.x=300;
- camara.position.z =300;
-  camara.position.y =-500;
+ camara.position.z =200;
+  camara.position.y =-400;
  
   /////////////////CAMARA/////////////////
 /* var campoVision=30;
